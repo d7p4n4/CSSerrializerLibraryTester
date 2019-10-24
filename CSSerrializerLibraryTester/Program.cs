8fb90a3e8d423ca4d868dcc30101992d2c1944ef
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-24 13:34:58 +0100

    1.20191024.5

--- CSSerrializerLibraryTester/Program.cs
+++ CSSerrializerLibraryTester/Program.cs
@@ -8,19 +8,38 @@ using CSSerializerLibrary;
 
 namespace CSSerrializerLibraryTester
 {
-    //2019. 10. 24. 14:04
+    //2019. 10. 24. 14:34
     class Program
     {
         private static readonly string PATH = ConfigurationManager.AppSettings["path"];
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly string APPSETTINGS_CLASSNAME = ConfigurationManager.AppSettings["className"];
+
+        private static string GetAppConfigStringParameter(string name)
+        {
+
+            return ConfigurationManager.AppSettings.Get(name);
+
+        }
+
         static void Main(string[] args)
         {
 
-            System.Reflection.Assembly a = System.Reflection.Assembly.Load("CSSerrializerLibraryTester");
-            var allTypes = a.GetTypes();
+            log.Debug("path:" + GetAppConfigStringParameter(APPSETTINGS_CLASSNAME));
 
-            foreach(var type in allTypes)
+            try
             {
-                Generate.SerializeClasses(type, PATH);
+
+                System.Reflection.Assembly a = System.Reflection.Assembly.Load("CSSerrializerLibraryTester");
+                var allTypes = a.GetTypes();
+
+                foreach (var type in allTypes)
+                {
+                    Generate.SerializeClasses(type, PATH);
+                }
+            } catch (Exception _exception)
+            {
+                log.Error(_exception.Message);
             }
         }
     }
