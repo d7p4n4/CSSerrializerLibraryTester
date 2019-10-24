9d13cab542757d9fa0793de508816c8f48afdd98
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-24 11:07:09 +0100

    Add project files.

---
+++ CSSerrializerLibraryTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSSerializerLibrary;
+
+namespace CSSerrializerLibraryTester
+{
+    //2019. 10. 24. 12:06
+    class Program
+    {
+        public const string PATH = CSSerializerLibrary.Program.PATH;
+        static void Main(string[] args)
+        {
+            Generate.SerializeClasses(typeof(Student), PATH);
+        }
+    }
+}
